fc7206b0b01d086fa29b66725a35f8f5bd5e3daf
ramarty <robmarty3@gmail.com>
2025-09-16 17:50:44 -0400

    run

--- 00_download_h5.R
+++ 00_download_h5.R
@@ -2,6 +2,37 @@
 bearer <- read_csv("~/Dropbox/bearer_bm.csv") %>%
   pull(token)
 
+# Monthly ----------------------------------------------------------------------
+product_id <- "VNP46A3"
+
+months_vec <- seq.Date(from = ymd("2012-01-01"),
+                       to = ymd("2025-07-01"),
+                       by = "month") %>%
+  as.character()
+
+for(date_i in months_vec){
+  message(date_i)
+  
+  dir.create(file.path(h5_root_dir, product_id, date_i))
+  
+  h5_dir <- file.path(h5_root_dir, product_id, date_i)
+  
+  # There are at most 540 files per date. If all downloaded, then skip
+  n_h5_date <- h5_dir %>%
+    list.files(pattern = "*.h5") %>%
+    length()
+  
+  if(n_h5_date < 540){
+    download_h5_files(roi_sf = NULL,
+                      product_id = product_id,
+                      date = date_i,
+                      h5_dir = h5_dir, 
+                      bearer = bearer,
+                      download_method = "httr")
+  }
+  
+}
+
 # Annual -----------------------------------------------------------------------
 product_id <- "VNP46A4"
 for(date_i in 2012:2024){
@@ -19,27 +50,6 @@ for(date_i in 2012:2024){
   
 }
 
-# Monthly ----------------------------------------------------------------------
-product_id <- "VNP46A3"
 
-months_vec <- seq.Date(from = ymd("2012-01-01"),
-                       to = ymd("2025-07-01"),
-                       by = "month") %>%
-  as.character()
-
-for(date_i in months_vec){
-  
-  dir.create(file.path(h5_root_dir, product_id, date_i))
-  
-  h5_dir <- file.path(h5_root_dir, product_id, date_i)
-  
-  download_h5_files(roi_sf = NULL,
-                    product_id = product_id,
-                    date = date_i,
-                    h5_dir = h5_dir, 
-                    bearer = bearer,
-                    download_method = "httr")
-  
-}
 
 
--- 01_make_rasters.R
+++ 01_make_rasters.R
@@ -10,21 +10,18 @@ adm0_sf <- read_sf(file.path(wb_bound_dir, "World Bank Official Boundaries - Adm
 #### Loop: Product
 for(product_id_i in c("VNP46A3", "VNP46A4")){ # "VNP46A3"
   
-  if(product_id_i == "VNP46A3") date_vec <- seq.Date(from = ymd("2012-01-01"),
-                                                     to = ymd("2025-07-01"),
-                                                     by = "month") %>%
-      as.character()
-  if(product_id_i == "VNP46A4") date_vec <- 2012:2024
+  date_vec <- file.path(h5_root_dir, product_id_i) %>%
+    list.files()
   
   #### Loop: Country
-  for(uid_i in sort(unique(adm0_sf$ISO_A3))){
+  for(uid_i in rev(sort(unique(adm0_sf$ISO_A3)))){
     
     #### Loop: Date
-    for(date_i in date_vec){
+    for(date_i in rev(date_vec)){
       
       #### H5 Directory
-      dir.create(file.path(h5_root_dir, product_id_i))
-      dir.create(file.path(h5_root_dir, product_id_i, date_i))
+      # dir.create(file.path(h5_root_dir, product_id_i))
+      # dir.create(file.path(h5_root_dir, product_id_i, date_i))
       
       h5_out_dir <- file.path(h5_root_dir, product_id_i, date_i)
       
@@ -79,7 +76,7 @@ for(product_id_i in c("VNP46A3", "VNP46A4")){ # "VNP46A3"
           })
         
         # showConnections(all = TRUE)
-        closeAllConnections()
+        # closeAllConnections()
         
         ##
       }
